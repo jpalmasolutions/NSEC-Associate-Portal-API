e87bbcec9c510571685d9193f268347c2348e088
jefry-palma <jefrypalmamartinez@gmail.com>
2021-11-22 10:54:11 -0800

    Adding different status codes

--- src/main/driver.py
+++ src/main/driver.py
@@ -3,9 +3,17 @@ from src.main.utils.logs import logger
 
 def lambda_handler(event,context):
 
-    logger.info(event)
+    try:
 
-    return {
-        "statusCode": 404,
-        "body": json.dumps('Cheers from AWS Lambda!!')
-    }
+        logger.info(event)
+
+    except Exception as e:
+        return {
+            "statusCode": 404,
+            "body": json.dumps('Error in Lambda')
+        }
+    else:
+        return {
+            "statusCode": 200,
+            "body": json.dumps('Cheers from AWS Lambda!!')
+        }
