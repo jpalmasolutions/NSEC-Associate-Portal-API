cb989b8295ce96f487c760c787da435b78d5da2f
jefry-palma <jefrypalmamartinez@gmail.com>
2021-11-22 10:44:45 -0800

    Adding return response with lambda proxy feature

--- src/main/driver.py
+++ src/main/driver.py
@@ -1,5 +1,11 @@
+import json
 from src.main.utils.logs import logger
 
 def lambda_handler(event,context):
 
     logger.info(event)
+
+    return {
+        "statusCode": 404,
+        "body": json.dumps('Cheers from AWS Lambda!!')
+    }
